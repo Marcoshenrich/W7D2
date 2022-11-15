ef2cf3309b1776de61f0cd8a1040555016487a1a
Marcos <jmarcoshenrich@gmail.com>
2022-11-15 10:57:59 -0800

    Users table complete

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_185407) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
