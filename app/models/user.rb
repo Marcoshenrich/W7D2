85caf17ecded7545c4aa2c2de888fae8e067733c
Marcos <jmarcoshenrich@gmail.com>
2022-11-15 12:44:09 -0800

    CRLLL methods written and validated

---
+++ app/models/user.rb
@@ -0,0 +1,60 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length:{minimum: 6}, allow_nil: true
+    before_validation :ensure_session_token
+    attr_reader :password
+
+    # SPIRE
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        pass_obj = BCrypt::Password.new(self.password_digest)
+        pass_obj.is_password?(password)
+    end
+
+    def reset_session_token
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    def generate_unique_session_token
+        token = 0
+        loop do
+            token = SecureRandom::urlsafe_base64
+            break unless User.exists?(session_token: token)
+        end
+        token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+end
